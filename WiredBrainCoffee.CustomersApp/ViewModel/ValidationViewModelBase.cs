2187480c557f49e705b73d476d5072f114edbaa7
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-12-20 15:48:42 +0100

    Add methods to add and remove errors

--- WiredBrainCoffee.CustomersApp/ViewModel/ValidationViewModelBase.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/ValidationViewModelBase.cs
@@ -8,10 +8,10 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
     public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
     {
-        private readonly Dictionary<string, List<string>> _errorssByPropertName = new();
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new();
         
         // it returns true if the object has errors
-        public bool HasErrors => _errorssByPropertName.Any();
+        public bool HasErrors => _errorsByPropertyName.Any();
 
         // it can be raised, and the the data binding will call again the GetErrors() method
         public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
@@ -19,15 +19,37 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
         // it returns a collection of errors for specific properties
         public IEnumerable GetErrors(string? propertyName)
         {
-            return propertyName is not null && _errorssByPropertName.ContainsKey(propertyName)
-                ? _errorssByPropertName[propertyName]
+            return propertyName is not null && _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName]
                 : Enumerable.Empty<string>();
         }
 
         // to raise the ErrorsChanged event
-        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs? e)
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e) => ErrorsChanged?.Invoke(this, e);      
+        
+        protected void AddError(string error, string propertyName)
         {
-            ErrorsChanged?.Invoke(this, e);
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName[propertyName] = new List<string>();
+            }
+
+            if (!_errorsByPropertyName.ContainsKey(error))
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+                RaisePropertyChanged(nameof(HasErrors));
+            }
+        }
+
+        protected void ClearErrors(string propertyName)
+        {
+            if (_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName[propertyName].Remove(propertyName);
+                OnErrorsChanged(new DataErrorsChangedEventArgs(propertyName));
+                RaisePropertyChanged(nameof(HasErrors));
+            }
         }
     }
 }
